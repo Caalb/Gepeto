27ef59041e19154a9035324037b7a52ebe88e29f
Caalb <carlosmunizfjunior@gmail.com>
2023-07-13 23:04:38 -0300

    feat(App.vue): adiciona loading bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <NConfigProvider :theme="theme">
+    <NLoadingBarProvider>
+      <router-view />
+    </NLoadingBarProvider>
+  </NConfigProvider>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+import {
+  darkTheme,
+  GlobalTheme,
+  NConfigProvider,
+  NLoadingBarProvider,
+} from "naive-ui";
+
+export default defineComponent({
+  components: {
+    NConfigProvider,
+    NLoadingBarProvider,
+  },
+
+  setup() {
+    return {
+      darkTheme,
+      theme: ref<GlobalTheme | null>(null),
+    };
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  background-color: #444444;
 }
 </style>
